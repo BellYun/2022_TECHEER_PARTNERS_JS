0f65cab1b3e2486666504d5b21c18d2cd3191697
BellYun <95991290+BellYun@users.noreply.github.com>
2022-12-24 09:37:30 +0900

    수정

--- 8/8.1/JS/weather.js
+++ 8/8.1/JS/weather.js
@@ -1,6 +1,6 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const weather = document.querySelector("#weather span:first-child");//weather의 자식을 객체로 받아옥
+const city = document.querySelector("#weather span:last-child");//weweath의 자식을 객체로 받아오기
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";//API수정 필요
 function onGeoOk(position) { //현재 위치 값 출력
     const lat = position.coords.latitude; 
     const lon = position.coords.longitude; 
